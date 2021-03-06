d23c7a72743b3d3236ef5d5e9d08f1e43043e037
Elijah Samuels <elijahsamuels@gmail.com>
2021-03-06 12:02:21 -0700

    added const for form

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ const baseURL = "http://localhost:3000/api/v1/plants"
 const plantsContainer = document.getElementById('plants-container')
 const plantsList = document.getElementById('plants-list')
 
-const plantSubmitForm = document.getElementById('plant-fsubmit-form')
-
+// Plant form fields
+const plantSubmitForm = document.getElementById('plant-submit-form')
 const plantSubmitName = document.getElementById('plant-submit-name')
+const plantSubmitHeight = document.getElementById('plant-submit-height')
+const plantSubmitGrowZone = document.getElementById('plant-submit-grow-zone')
 const plantSubmitNotes = document.getElementById('plant-submit-notes')
+const plantSubmitSensor = document.getElementById('plant-submit-sensor')
+const plantSubmitSenorMacAddress = document.getElementById('plant-submit-sensor-macaddress')
+const plantSubmit = document.getElementById('plant-submit')
 
 document.addEventListener('DOMContentLoaded', () => {
 	getPlants();
